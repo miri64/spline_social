18b9517716cc1d65f7c482b2cc3d0641e71a7486
Authmillenon <martin@ucsmail.de>
2011-10-13 16:20:24 +0200

    Change order of defininitions

--- db.py
+++ db.py
@@ -6,21 +6,6 @@ from hashlib import sha256 as sha
 
 Base = declarative_base()
 
-class DBConn(object):
-    def __new__(type, *args, **kwargs):
-        if not '_the_instance' in type.__dict__:
-            type._the_instance = object.__new__(type)
-        return type._the_instance
-    
-    def __init__(self, name = None):
-        if name != None:
-            self.engine = sqlalchemy.create_engine('sqlite:///%s' % name)
-            Base.metadata.create_all(self.engine)
-    
-    def get_session(self):
-        Session = sqlalchemy.orm.sessionmaker(bind=self.engine)
-        return Session()
-
 class User(Base):
     __tablename__ = 'user'
     user_id = sqlalchemy.Column(sqlalchemy.String, primary_key=True)
@@ -68,3 +53,18 @@ class User(Base):
     
     def validate_password(self,password):
         return self.password == self._get_pwhash(password)
+
+class DBConn(object):
+    def __new__(type, *args, **kwargs):
+        if not '_the_instance' in type.__dict__:
+            type._the_instance = object.__new__(type)
+        return type._the_instance
+    
+    def __init__(self, name = None):
+        if name != None:
+            self.engine = sqlalchemy.create_engine('sqlite:///%s' % name)
+            Base.metadata.create_all(self.engine)
+    
+    def get_session(self):
+        Session = sqlalchemy.orm.sessionmaker(bind=self.engine)
+        return Session()
