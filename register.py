b2833f68eacd568af839835234152fa439211ec1
Authmillenon <martin@ucsmail.de>
2011-10-24 22:30:56 +0200

    Änderungen an Datenbankschema für register script berücksichtigt

--- register.py
+++ register.py
@@ -6,24 +6,23 @@ from xmlrpclib import ServerProxy
 def register(rpc):
     uid = getpass.getuser()
     username = raw_input(u"Dein Spline-Username [%s]: " % uid)
-    spline_username = uid if username == '' else username
+    username = uid if username == '' else username
     spline_password = getpass.getpass('Dein LDAP-Passwort: ')
-    username = raw_input(u"Waehle deinen Benutzernamen fuer den IRC-Bot [%s]: " % spline_username)
     irc_username = uid if username == '' else username
     irc_password = 0
     irc_password2 = 1
     while irc_password != irc_password2:
-        irc_password = getpass.getpass(u'Waehle ein Passwort fuer die Anmeldung: ')
+        irc_password = getpass.getpass(u'Waehle ein Passwort um dich beim Bot zu identifizieren: ')
         irc_password2 = getpass.getpass('Wiederholung: ')
         if irc_password != irc_password2:
             print u"Passwoerter stimmen nicht ueberein."
-    print "Willst du per Mail (%s@spline.inf.fu-berlin.de) informiert werden, wenn du einen Post schreibst? [y/N] " % spline_username,
+    print "Willst du per Mail (%s@spline.inf.fu-berlin.de) informiert werden, wenn du einen Post schreibst? [y/N] " % username,
     get_mail_char = sys.stdin.read(1)
     if get_mail_char.lower() == 'y':
         get_mail = True
     else:
         get_mail = False
-    success = rpc.add_user(spline_username, spline_password, irc_password, irc_username, get_mail)
+    success = rpc.add_user(username, spline_password, irc_password, get_mail)
     if success:
         print "Anmeldung erfolgreich."
     else:
