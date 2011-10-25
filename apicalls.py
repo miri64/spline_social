0df3a3d5f605bb7163b4ecfb90ef98c3b26eb7eb
Authmillenon <martin@ucsmail.de>
2011-10-25 15:31:42 +0200

    Fixed sendmail

--- apicalls.py
+++ apicalls.py
@@ -120,7 +120,7 @@ class IdenticaApi(identica.Api):
             user.add_post(status, source)
             if user.gets_mail:
                 try:
-                    send_information_mail(user, status)
+                    self._send_information_mail(user, status)
                 except BaseException, e:
                     print 'Error in send_infomation_mail:', type(e).__name__, e
                     exit(1)
